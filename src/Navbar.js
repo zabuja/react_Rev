0fb1ece5c94deb7efa076048f5cb1e449c19d6f2
zabuja <atanu.tmsl@gmail.com>
2022-05-30 20:31:14 +0530

    changes

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">Create Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
