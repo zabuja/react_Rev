10c4f58a1b4bcf32e2f2cff5fc5aace35402af1b
zabuja <atanu.tmsl@gmail.com>
2022-06-12 23:48:56 +0530

    lesson 14

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 
@@ -14,6 +14,10 @@ const Home = () => {
        const newBLogs =  blogs.filter(blog => blog.id !== id);
        setBlogs(newBLogs);
     }
+
+    useEffect(() => {
+        console.log('use effct ran');
+    });
 
     return ( 
         <div className="home">
