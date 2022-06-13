3278d068100dfe00c0d95854ac9149bc94637764
zabuja <atanu.tmsl@gmail.com>
2022-06-13 12:04:18 +0530

    lesson18

--- src/Home.js
+++ src/Home.js
@@ -6,20 +6,24 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
 
-
+    const[isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
                return res.json()
             })
             .then(data => {
-                setBlogs(data)
-            })
+                setBlogs(data);
+                setIsPending(false);
+            });
+        }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+            {isPending && <div>loading...</div>}
             {blogs && <BlogList blogs = {blogs} title = "All blogs!"  />} 
         </div>
      );
