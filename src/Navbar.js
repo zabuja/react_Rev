2ffceb06e3c106129732ad498718eba61f279b2c
zabuja <atanu.tmsl@gmail.com>
2022-06-16 21:51:48 +0530

    modified

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <h1>The Blong</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create">Create Blog</Link>
